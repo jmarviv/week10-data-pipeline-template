d668de084ed4556528d79beb60cb031c641d4f1f
jmarviv <jorgemarcosvives@gmail.com>
2026-05-06 11:40:19 +0200

    I changed the prints for logging infos

--- scripts/report.py
+++ scripts/report.py
@@ -10,13 +10,13 @@ try:
     # Ensure output directory exists
     os.makedirs("output", exist_ok=True)
 
-    print("Loading data...")
+ #   print("Loading data...")
     logging.info("Loading data...")
 
     df = pd.read_parquet("data/sample_data.parquet")
     #df.loc[0, "price"] = -10.0 Ha ido y ha saltado el error en la pipeline
 
-    print("Processing data...")
+#    print("Processing data...")
     logging.info("Processing data...")
 
     if (df["price"] < 0).any():
@@ -33,12 +33,12 @@ try:
         transaction_count=("qty", "count")
     ).reset_index()
 
-    print("Saving report...")
+#    print("Saving report...")
     logging.info("Saving report...")
     summary.to_csv("output/report.csv", index=False)
 
-    print("✅ Report generated at output/report.csv")
-    print("Report generation completed successfully.")
+  #  print("✅ Report generated at output/report.csv")
+    logging.info("Report generation completed successfully.")
 
 except Exception as e:
     logging.error(f"An error occurred: {e}")
